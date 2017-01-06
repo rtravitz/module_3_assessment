7f2cb72417a805020b040828b25a68791130629c
Ryan Travitz <rtravitz@gmail.com>
2017-01-06 12:11:21 -0700

    final push

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
   def show
     id = params[:id]
-    @store_info = BestbuyService.new.store_info(id)[:stores].first
-    @hours = @store_info[:hoursAmPm].split(";")
+    @store = BestbuyService.new.store_info(id)[:stores].first
+    @hours = @store[:hoursAmPm].split(";")
   end
 end
--- spec/features/search/user_can_see_an_individual_store_info_spec.rb
+++ spec/features/search/user_can_see_an_individual_store_info_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 describe "As a user who clicks on a store" do
   it "I can see its information" do
     VCR.use_cassette("single_store") do
-      visit "/stores/1171"
+      visit "/"
+      fill_in "zipcode", with: "80202"
+      click_on "Search"
+      click_on "Southglenn"
 
       expect(current_path).to eq("/stores/1171")
       expect(page).to have_content("BigBox")
       expect(page).to have_content("Centennial")
       expect(page).to have_content("303-797-3246")
+      expect(page).to have_content("Mon: 10am-9pm")
     end
   end
 end
