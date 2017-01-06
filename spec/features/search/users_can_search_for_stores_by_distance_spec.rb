36a35d1242742fa422896f4b9affe1f6384bb891
Ryan Travitz <rtravitz@gmail.com>
2017-01-06 10:27:03 -0700

    Write test for consuming api

---
+++ spec/features/search/users_can_search_for_stores_by_distance_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "As a user when I use the search box" do
+  it "I can see stores within a specified distance" do
+    VCR.use_cassette("store_distance") do
+      visit "/"
+      fill_in "search_box", with: "80202"
+      expect(current_path).to eq("/search")
+      expect(page).to have_content("Stores")
+      expect(page).to have_content("16 Total Stores")
+      expect(page).to have_content("Centennial")
+      expect(page).to have_content("BigBox")
+      expect(page).to have_content("303-797-3246")
+      expect(page).to have_content("15")
+    end
+  end
+end
